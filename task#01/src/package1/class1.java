487914a28752e41bacf83afe57be510437898824
Randika Sandaruwan <Randika Sandaruwan@DESKTOP-FVMLB8H>
2020-06-10 02:05:16 +0530

    t01

---
+++ task#01/src/package1/class1.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class class1 {
+    private static final String FILE_PATH = "C:/Users/Randika Sandaruwan/Desktop/programming-help-ex/task#01/src/list4.json";
+    
+    public static void main(String[] args) {
+    	class2 reader = new class2();
+        reader.readData(FILE_PATH);
+    }
+}
